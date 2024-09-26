3fff3c94dfb0973d3cc4d6425d8c9201ea61c614
LucianoCoimbraJ <lucianojuniorph@gmail.com>
2024-09-26 17:32:07 -0300

    Trabalho do Rafael sobre farmacia

---
+++ Farmacia/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Homepage from './componets/Homepage.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ Farmacia/src/componets/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Homepage.css'
+
+function Homepage() {
+  return (
+    <div className='Nav-Bar'>
+      <button>Home</button>
+      <button>Senha</button>
+      <button>Sobre-Mim</button>
+    </div>
+  )
+}
+
+export default Homepage
